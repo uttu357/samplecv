25822aca70cb5ca3a8979128ac57dcc7611ab2c9
Ashish Chaudhary <me@ashishchaudhary.in>
2016-06-10 07:53:27 +0530

    sample commit for no reason

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu:14.04
+#RUN apt-get update && apt-get install -y python-pip python-opencv && pip install flask
+ADD app.py /
+ENV FLASK_APP app.py
+EXPOSE 5000
+ENTRYPOINT ln -s /dev/null /dev/raw1394;flask run --host=0.0.0.0 --port=5000
+
---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, abort, request
+import cv2
+app = Flask(__name__)
+
+@app.route('/')
+def serve_form():
+    to_serve = '''<html><body>
+                        <form id="upload-file" method="post" enctype="multipart/form-data">
+                                <label for="file">Select a file</label>
+                                <input name="file" type="file">
+                                <button id="upload-file-btn" type="button">Upload</button>
+                        </form>
+                        <img id='updateMe' src='' />
+                        <script src="https://code.jquery.com/jquery-2.x-git.min.js"></script>
+                    </body>
+                    </html>'''
+    return to_serve
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      if f.content_type == 'image/jpeg' or f.content_type == 'image/jpg' or f.content_type == 'image/png':
+          f.save('/tmp/' + f.filename)
+          image = cv2.imread('/tmp/' + f.filename)
+          gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+          cv2.imwrite('/tmp/' + f.filename.split('.jpg')[0] + '_modified.jpg', gray_image)
+          with open('/tmp/' + f.filename.split('.jpg')[0] + '_modified.jpg', 'rb') as file:
+              if (f.content_type == 'image/jpeg' or f.content_type == 'image/jpg'):
+                ff = 'data:image/jpeg;base64,' + file.read().encode("base64")
+              elif (f.content_type == 'image/png'):
+                  ff = 'data:image/png;base64,' + file.read().encode("base64")
+              return ff
+      else:
+          abort(400)
