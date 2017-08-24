1504d8174abcb6e254ce5c7f4f2d14cd2492a744
Utkarsh Gupta <utkarsh357@gmail.com>
2017-08-24 06:59:18 +0530

    Working sample demo

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,17 @@
 FROM ubuntu:14.04
-RUN apt-get update && apt-get install -y python-pip python-opencv && pip install flask && pip install tornado
-RUN pip install -U flask-cors
+
+RUN rm /bin/sh && ln -s /bin/bash /bin/sh
+RUN apt-get update && \
+    apt-get install -y python-pip python2.7 python2.7-dev python-opencv
+RUN pip install --upgrade pip
+RUN pip install virtualenv
+ADD requirements.txt /
 ADD app.py /
 ADD cvfy.py /
-ENTRYPOINT python app.py
+RUN virtualenv ./venv
+RUN /bin/bash -c "source /venv/bin/activate \
+    && pip install -r requirements.txt"
+RUN cp /usr/lib/python2.7/dist-packages/cv2.so /venv/lib/python2.7/site-packages/
 
+ENTRYPOINT source /venv/bin/activate \
+           && python app.py
--- app.py
+++ app.py
@@ -1,21 +1,39 @@
-
+import cv2
 import cvfy
 
-app = cvfy.register('gh:107.170.77.168:60819434:3000:8000')
+app = cvfy.register('nongh:0.0.0.0:9290704:8000:9001:0.0.0.0:8000')
 
 @cvfy.crossdomain
 @app.listen()
-def concat():
+def grayscale():
+
+    all_image_paths = cvfy.getImageArray()
+#    all_texts = cvfy.getTextArray()
+    image_1_path = all_image_paths[0]
+    image_2_path = all_image_paths[1]
+#    text_1 = all_texts[0]
+#    text_2 = all_texts[1]
         
-    ## receiving the data
-    alltext = cvfy.getTextArray()
-    
-    ## processing the data
-    joined_and_uppercase = (alltext[0] + ' ' + alltext[1]).upper()
-    and_then_reversed = joined_and_uppercase[::-1]
-    
-    ## sending back the data
-    cvfy.sendTextArray([joined_and_uppercase, and_then_reversed])
+    cvfy.sendTextArrayToTerminal(['Loading Image...'])
+    image_1 = cv2.imread(image_1_path)
+    image_2 = cv2.imread(image_2_path)
+
+    cvfy.sendTextArrayToTerminal(['Image Loaded successfully']);
+
+    gray_image_1 = cv2.cvtColor(image_1, cv2.COLOR_BGR2GRAY)
+    gray_image_2 = cv2.cvtColor(image_2, cv2.COLOR_BGR2GRAY)
+
+    cvfy.sendTextArrayToTerminal(['Converting RGB Image to Grayscale']);
+
+#    cvfy.sendTextArray(["sucess", "falure"])
+    cvfy.sendImageArray([gray_image_1, gray_image_2], mode = 'numpy_array')
+
+    cvfy.sendTextArrayToTerminal([
+        'Operation completed successfully',
+        image_1_path,
+        image_2_path
+        ]);
+
     return 'OK'
-        
+
 app.run()
